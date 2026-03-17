a5678d1cdb619eaa7e5985906c9628d8aca45be0
Unnati Pitla <unnatipitla1026@gmail.com>
2026-03-17 19:34:02 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
-console.log("Dashboard Loaded");
+import { dashboardData } from "./data.js"
+
+document.getElementById("users").textContent = dashboardData.users
+
+document.getElementById("revenue").textContent = "$" + dashboardData.revenue
+
+document.getElementById("orders").textContent = dashboardData.orders
+
+document.getElementById("growth").textContent = dashboardData.growth + "%"
+
+
+console.log("Dashboard Data Loaded:", dashboardData)
+
