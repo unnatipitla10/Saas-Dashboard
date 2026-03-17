b0bdb4389f514783ce2192b5d254d0674fac2dac
Unnati Pitla <unnatipitla1026@gmail.com>
2026-03-17 19:34:51 +0530

    Create data.js

---
+++ JS/data.js
@@ -0,0 +1,28 @@
+export const dashboardData = {
+
+users: 1245,
+
+revenue: 8420,
+
+orders: 320,
+
+growth: 12,
+
+salesData: [
+{month:"Jan",sales:400},
+{month:"Feb",sales:520},
+{month:"Mar",sales:610},
+{month:"Apr",sales:700},
+{month:"May",sales:830},
+{month:"Jun",sales:920}
+],
+
+userActivity: [
+{name:"John",orders:12},
+{name:"Sara",orders:8},
+{name:"Alex",orders:15},
+{name:"Emma",orders:6}
+]
+
+}
+
